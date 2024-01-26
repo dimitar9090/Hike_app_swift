6a373f70e3b4d6abf25906737f6211dce5622898
Dimitar Anguelov   <113285865+dimitar9090@users.noreply.github.com>
2024-01-27 00:22:23 +0200

    Update CustomBackgroundView.swift

--- Component/CustomBackgroundView.swift
+++ Component/CustomBackgroundView.swift
@@ -9,20 +9,20 @@ import SwiftUI
 struct CustomBackgroundView: View {
   var body: some View {
     ZStack {
-      // MARK: - 3. DEPTH
+      
       
       Color.customGreenDark
         .cornerRadius(40)
         .offset(y: 12)
       
-      // MARK: - 2. LIGHT
+      
       
       Color.customGreenLight
         .cornerRadius(40)
         .offset(y: 3)
         .opacity(0.85)
       
-      // MARK: - 1. SURFACE
+     
       
       LinearGradient(
         colors: [
